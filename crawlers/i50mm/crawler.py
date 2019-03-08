281c73361727b2867f771f59c0f71130cb3645e5
Mingpeiyu Zhang <zmpyzmpy@gmail.com>
2019-03-08 12:23:13 +0800

    refactor i50mm

--- crawlers/i50mm/crawler.py
+++ crawlers/i50mm/crawler.py
@@ -1,63 +1,48 @@
 from bs4 import BeautifulSoup
 import time
-import glob
 import requests
 import os
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+
+
+def crawl_one_page(url, out_file):
+    try:
+        re = requests.get(url, headers=headers)
+    except:
+        time.sleep(1)
+    re.encoding = 'utf-8'
+    article_soup = BeautifulSoup(re.text, 'html.parser')
+    article = article_soup.find("div", {"class": "entry-content"})
+    out_file.write(article.get_text())
+    out_file.flush()
+    next_page = article_soup.find("div", {"class", "nav-previous"}).find("a", href=True)['href']
+
+    return next_page
+
+
 def main():
-	headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    count = 1
+    start_url = 'http://i50mm.com/?p=5189'
+    name = "./out/articles/" + str(count) + '.txt'
+    directory = os.path.dirname(name)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    out_file = open(name, 'w')
+
+    next_page = crawl_one_page(start_url, out_file)
+
+    while next_page:
+        print(count)
+        count += 1
+        name = "./out/articles/" + str(count) + '.txt'
+        out_file = open(name, 'w')
+
+        next_page = crawl_one_page(next_page, out_file)
+
+    print('done!')
 
 
-	count = 1
-	
-	
-	start_url = 'http://i50mm.com/?p=5189'
-	re = requests.get(start_url, headers=headers)
-	name = "./out/articles/" + str(count) + '.txt'
-	
-	directory = os.path.dirname(name)
-	if not os.path.exists(directory):
-		os.makedirs(directory)
-	out_file = open(name, 'w')
-	re.encoding = 'utf-8'
-	article_soup = BeautifulSoup(re.text, 'html.parser')
-	article = article_soup.find("div",{"class":"entry-content"})
-	out_file.write(article.get_text())				
-	out_file.flush()
-	
-	next = article_soup.find("div",{"class","nav-previous"})
-	next = next.find("a",href=True)['href']
-	while next:
-		print(count)
-		count += 1
-		
-		
-		url = next
-		name = "./out/articles/" + str(count) + '.txt'
-		
-		
-		try:
-			re = requests.get(url, headers=headers)
-		except:
-			time.sleep(1)
-			continue
-		directory = os.path.dirname(name)
-		if not os.path.exists(directory):
-			os.makedirs(directory)
-		out_file = open(name, 'w')
-		re.encoding = 'utf-8'
-		article_soup = BeautifulSoup(re.text, 'html.parser')
-		
-		article = article_soup.find("div",{"class":"entry-content"})	
-		if article == None:
-			print(url)
-			continue		
-		
-		out_file.write(article.get_text())				
-		out_file.flush()
-		next = article_soup.find("div",{"class","nav-previous"})
-		next = next.find("a",href=True)['href']
-
-			
-	
 if __name__ == '__main__':
-	main()
+    main()
